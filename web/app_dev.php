bcadb12254a155437f78c00a3b0861ff93ef50bb
stephanschmid <stephanbschmid@gmail.com>
2014-09-08 17:15:46 +0200

    edge5 banner animations

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-        || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.1.72', '192.168.1.66','192.168.1.118','192.168.1.50','192.168.1.50','192.168.1.41','192.168.1.90')) || php_sapi_name() === 'cli-server')
+        || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.1.72', '192.168.1.66','192.168.1.118','192.168.1.50','192.168.1.50','192.168.1.41','192.168.1.90','192.168.1.58','192.168.1.94')) || php_sapi_name() === 'cli-server')
 {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
